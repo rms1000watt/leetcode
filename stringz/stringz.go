b6835f55f2f3c330d9d6fb112ea830bf84de86e0
Ryan M Smith <rms1000watt@gmail.com>
2022-04-08 23:11:18 -0700

    running tag-push.sh

--- stringz/stringz.go
+++ stringz/stringz.go
@@ -7,3 +7,21 @@ func Reverse(in string) (out string) {
 	}
 	return
 }
+
+// Palindrome checks if string is a palindrome
+func Palindrome(in string) (out bool) {
+	if len(in) == 1 {
+		out = true
+		return
+	}
+
+	for i := 0; i < (len(in)/2)+1; i++ {
+		j := len(in) - 1 - i
+		if in[i] != in[j] {
+			return
+		}
+	}
+
+	out = true
+	return
+}
